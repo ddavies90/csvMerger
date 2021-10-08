efb9568c65413d8ab301eab3530707cdf98f201e
David Davies <d.davies90@hotmail.co.uk>
2021-10-08 08:22:57 +0100

    Looped over file two and appended availability data to the values for file one. Created new records if none exist.

--- main.go
+++ main.go
@@ -7,15 +7,22 @@ import (
 	"os"
 )
 
+type item struct {
+	id           string
+	size         string
+	label        string
+	availability string
+}
+
 func main() {
-	a := map[string]item{}
-	//i := item{id: "2wqerqjo1-21141", size: "XL", label: "Extra Large"}
-	f1, err := os.Open("./fileSet1/file1.csv")
+	items := map[string]item{}
+	file1, err := os.Open("./fileSet1/file1.csv")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer f1.Close()
-	csvReader := csv.NewReader(f1)
+	defer file1.Close()
+
+	csvReader := csv.NewReader(file1)
 	data, err := csvReader.ReadAll()
 	if err != nil {
 		log.Fatal(err)
@@ -23,16 +30,47 @@ func main() {
 
 	for i := 1; i < len(data); i++ {
 		itemData := item{id: data[i][0], size: data[i][1], label: data[i][2]}
-		a[itemData.id] = itemData
+		items[itemData.id] = itemData
 	}
 
-	fmt.Println(a)
+	file2, err := os.Open("./fileSet1/file2.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file2.Close()
 
-}
+	csvReader = csv.NewReader(file2)
+	data, err = csvReader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-type item struct {
-	id           string
-	size         string
-	label        string
-	availability string
+	for i := 1; i < len(data); i++ {
+		itemData := item{id: data[i][0], availability: data[i][1]}
+		if val, ok := items[itemData.id]; ok {
+			fmt.Println(val)
+			val.availability = itemData.availability
+		} else {
+			items[itemData.id] = itemData
+		}
+	}
+
+	//set testItem to the result of searching the map for the value passed in to the [] which is a unique ID - should
+	//return a single record. Append availability property on to this record.
+	//testItem := items["fff94c24-2700-4332-af22-f423091100a9"]
+	//testItem.availability = "Available"
+
+	//Search items map to find the record that has the ID that testItem has and set it to the updated testItem, as we
+	//just amended it by adding availability.
+	//items[testItem.id] = testItem
+
+	csvfile, err := os.Create("MergedCSV.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	csvwriter := csv.NewWriter(csvfile)
+
+	//fmt.Println(len(items))
+
 }
